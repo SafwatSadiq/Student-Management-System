2e93b1e396ca78b171d2a04f4a4f08e7c32050fc
Safwat Sadiq <Safwatsadiq14@gmail.com>
2024-09-26 13:53:52 +0600

    Added a table using QTableWidget and also added header to 4 columns

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import sys
+import sqlite3
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, \
-    QLineEdit, QPushButton, QMainWindow
+    QLineEdit, QPushButton, QMainWindow, QTableWidget
 from PyQt6.QtGui import QIcon, QAction
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Student Management System")
         self.setWindowIcon(QIcon.fromTheme("system-search"))
+        self.setGeometry(370, 150, 640, 480)
         
         # Set the menu bar
         file_menu_item = self.menuBar().addMenu("&File")
@@ -19,8 +22,15 @@ class MainWindow(QMainWindow):
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
         
+        # Add table
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("ID", "Name", "Course", "Mobile"))
+        self.setCentralWidget(self.table)
         
         
+    def load_data(self):
+        pass
         
 
 app = QApplication(sys.argv)
